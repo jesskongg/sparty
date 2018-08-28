f91eecf52a21131f649e8e1486885fe0af3cd6d5
Tu Dat Nguyen <nguyentudat@gmail.com>
2018-08-28 00:43:29 -0700

    fixed route similar path prefix

--- routes/api.js
+++ routes/api.js
@@ -5,23 +5,21 @@ var router = express.Router();
 var spotify_controller = require('../controllers/spotifyController');
 var room_controller = require('../controllers/roomController');
 
-// ?: the order of router matters ?
-
 router.get('/search', spotify_controller.spotify_search);
 
 router.get('/auth/login', spotify_controller.spotify_login);
 
 router.get('/auth/logout', spotify_controller.spotify_logout);
 
+router.get('/room/create', room_controller.room_create_get);
+
+router.post('/room/create', room_controller.room_create_post);
+
 router.get('/room/search', room_controller.room_search);
 
 router.get('/rooms', room_controller.room_list);
 
 router.get('/room/:id', room_controller.room_detail);
-
-router.get('/room/create', room_controller.room_create_get);
-
-router.post('/room/create', room_controller.room_create_post);
 
 router.get('/room/:id/update', room_controller.room_update_get);
 
