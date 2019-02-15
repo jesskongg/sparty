606f79956caadabd7d8195b3feefe3123f9fa967
Tu Dat Nguyen <nguyentudat@gmail.com>
2019-02-15 00:00:11 -0800

    Fix Search Song

--- controllers/spotifyController.js
+++ controllers/spotifyController.js
@@ -1,51 +1,63 @@
 var passport = require('passport');
-var http = require('http');
 var SpotifyWebApi = require('spotify-web-api-node');
 
-//Create a Spotify Client
-var spotifyApi = new SpotifyWebApi({
+var callbackURL = process.env.NODE_ENV === 'production' ? 'https://chardonnay.herokuapp.com/callback' : 'http://localhost:3000/callback';
+
+var credentials = {
   clientId: process.env.CLIENT_ID,
   clientSecret: process.env.CLIENT_SECRET,
-  // redirectUri: 'http://www.spartyfy.me/callback'
-  redirectUri: 'https://chardonnay.herokuapp.com/callback'
+  redirectUri: callbackURL
+}
+//Create a Spotify Client
+var clientSpotify = new SpotifyWebApi(credentials);
 
-});
+var tokenExpired = 0;
 
-spotifyApi.clientCredentialsGrant().then(
-  function(data) {
-    spotifyApi.setAccessToken(data.body['access_token']);
-  }
-);
-
-exports.spotify_search = function(req, res, next) {
-  //Set the type of query: track
-  var type = 'track';
-  var query = req.query.query;
-  //Clear out old results
-  var results = [];
-
-  //Make a request to Spotify
-  spotifyApi.searchTracks(query)
-      .then(function (spotRes) {
-        //Store the artist, song, preview link, and album in the results array
-        spotRes.body.tracks.items.forEach(function(ea){
-          var album_cover = (ea.album.images.length) ? ea.album.images[0].url : '';
-          results.push({
-                        artist: ea.artists[0].name,
-                        name: ea.name,
-                        id: ea.id,
-                        album: ea.album.name,
-                        image: album_cover,
-                        uri: ea.uri,
-            });
-        });
-        res.json(results);
+exports.spotify_search = function(req, res) {
+  if (Date.now()/1000 - tokenExpired > 0) {
+    clientSpotify.clientCredentialsGrant().then(
+      function(data) {
+        clientSpotify.setAccessToken(data.body['access_token']);
+        tokenExpired = Date.now()/1000 + data.body['expires_in'];
+        searchSong(req.query.query).then((songs) => {
+          res.json(songs);
+        })
+      }
+    );
+  } else {
+    searchSong(req.query.query).then((songs) => {
+      res.json(songs);
     })
-    .catch(function (err) {
-        console.log(err);
-        throw err;
-      });
+  }
 };
+
+function searchSong(query) {
+  return new Promise((resolve, reject) => {
+    //Clear out old results
+    var results = [];
+    //Make a request to Spotify
+    clientSpotify.searchTracks(query)
+        .then(function (spotRes) {
+          //Store the artist, song, preview link, and album in the results array
+          spotRes.body.tracks.items.forEach(function(ea){
+            var album_cover = (ea.album.images.length) ? ea.album.images[0].url : '';
+            results.push({
+                          artist: ea.artists[0].name,
+                          name: ea.name,
+                          id: ea.id,
+                          album: ea.album.name,
+                          image: album_cover,
+                          uri: ea.uri,
+              });
+          });
+          resolve(results);
+      })
+    .catch(function (err) {
+        reject(err);
+    });
+  })
+
+}
 
 exports.spotify_login = passport.authenticate('spotify', {
     // The request will be redirected to spotify for authentication, so this
@@ -64,19 +76,19 @@ exports.spotify_callback = function(req, res, next) {
     if (!user) { return res.redirect('/login'); }
     req.logIn(user, function(err) {
       if (err) { return next(err); }
-      spotifyApi.setAccessToken(user.access_token);
       return res.redirect('/login');
     })
   }) (req, res, next);
 };
 
 exports.spotify_get_access_token = function(req, res, next) {
-  spotifyApi.setRefreshToken(req.user.refresh_token);
-  spotifyApi.refreshAccessToken()
+  var userSpotifyApi = new SpotifyWebApi(credentials);
+  userSpotifyApi.setRefreshToken(req.user.refresh_token);
+  userSpotifyApi.refreshAccessToken()
   .then(function(data) {
         console.log('The access token has been refreshed!');
         // Save the access token so that it's used in future calls
-        spotifyApi.setAccessToken(data.body['access_token']);
+        userSpotifyApi.setAccessToken(data.body['access_token']);
         res.send(data.body['access_token']);
       }, function(err) {
         console.log('Could not refresh access token', err);
